3473183cf99176a5bc26c27cd750a9f197900ae6
Patrick Fernando <patrickfernando1234@gmail.com>
2022-12-13 16:44:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<= n; i++)
+            {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+            Console.WriteLine("Enter the employee id that will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find ( x => x.Id == searchId);
+            if (emp != null)
+            {
+                Console.WriteLine("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo. InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Update list of employees: ");
+   
+            foreach (Employee obj in list){
+                Console.WriteLine(obj);
+            }
+            Console.ReadLine();
+        }
+    }
+}
